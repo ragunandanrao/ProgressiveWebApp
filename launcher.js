283e2239fea6312329b31c4b2d6f90842a01370e
Raghu Rao <malangul@adobe.com>
2017-06-20 21:07:44 -0700

    Code for tutorial

--- launcher.js
+++ launcher.js
@@ -1,1 +1,70 @@
-console.log("I am JS file");
+function pushAskPermission() {
+  return new Promise(function(resolve, reject) {
+      //Notification is the API for browser notifications
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+ 
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+  .then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    }
+  });
+}
+
+function pushSubscribeUser() {
+  navigator.serviceWorker.getRegistration().then(
+      function(registration) {
+        const subscribeOptions = {
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(
+                'BDc9cbkk88rKFRYw7lfOdROs_oW4-u5UdFvS9jXdvikHkgLjmuY2RshWmUPLWOjrQAK-r-NflkTO5nWxWl3nSGQ'
+            )
+        };
+        return registration.pushManager.subscribe(subscribeOptions);
+  })
+  .then(function(pushSubscription) {
+    console.log('Push Subscription: ', JSON.stringify(pushSubscription));
+    return pushSubscription;
+  });
+}
+ 
+// Snippet from https://www.npmjs.com/package/web-push
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+ 
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+ 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+ 
+// Snippet from https://www.npmjs.com/package/web-push
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+ 
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+ 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+ 
+ 
+
--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,8 @@
 console.log("Hello from a SW");
 //Fetch is the first event that is called when we load any resource in the domain.
-var urls = ["/", "style.css", "launcher.js"];
+var urls = ["/", "style.css", "launcher.js","manifest.json",
+           "https://fonts.googleapis.com/icon?family=Material+Icons",
+           "https://fonts.gstatic.com/s/materialicons/v22/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2"];
  
 self.addEventListener("install", function(event) {
     console.log("The SW is now installed"); 
@@ -34,6 +36,68 @@ self.addEventListener('fetch', function(event) {
             })
         );
     });  
+
+self.addEventListener('activate', function(event) {
+ 
+  // Array of cache that we will use in this version
+  var cacheWhitelist = ["activityLauncher"];
+ 
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Deletes the cache because we won't use it here
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
+
+
+
+self.addEventListener('sync', function(event) {
+    if (event.tag == 'eurocheck') {
+        event.waitUntil(fetch("http://api.fixer.io/latest?base=USD").then(function (result){
+            return result.json();
+        })
+    .then(function (result) {
+            console.log(result.rates.EUR);
+        })
+    );
+    }
+});
+
+self.addEventListener("push", function(event) {
+  if (event.data) {
+    console.log('Push Data: ', event.data.text());
+  } else {
+    console.log('No data :(');
+  }
+  self.registration.showNotification("Push title", {
+      //options for the notifications like body , actions, icon, badge etc
+        body: event.data.text()
+  });
+});
+
+self.addEventListener('notificationclick', function(event) {
+ if (!event.action) {
+   console.log('Notification Click with no action');
+   return;
+ } else {
+   // event action has the action id
+ }
+self.registration.showNotification("Push title", {
+       body: event.data.text()
+ });
+ event.notification.close();
+ 
+ event.waitUntil(doSomething);
+ 
+});
+
 
 
 //self.addEventListener("fetch", function(event) {
